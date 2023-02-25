830473690d05600b736009de5cd8c1d7b6f2691e
Paul <82803315+ppauel@users.noreply.github.com>
2023-02-25 12:58:06 +0100

    Added simple router

--- src/index.js
+++ src/index.js
@@ -1,8 +1,22 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
+import {
+    createBrowserRouter,
+    createRoutesFromElements,
+    Navigate,
+    Route,
+    RouterProvider,
+} from "react-router-dom";
 import './style/main.css';
 import App from './App';
 
+// Router
+const router = createBrowserRouter(
+    createRoutesFromElements(
+        <Route path="/" element={<App />} errorElement={<Navigate replace to="/" />} />
+    )
+);
+
 ReactDOM.createRoot(document.getElementById('root')).render(
-        <App />
+    <RouterProvider router={router} />
 );
