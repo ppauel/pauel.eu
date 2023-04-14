764fe9db1d089ea8ee6620b063e9f5699fafb0ae
ppauel <mail@pauel.eu>
2023-04-14 17:06:59 +0200

    updated descriptions

--- src/sections/Projects.jsx
+++ src/sections/Projects.jsx
@@ -10,18 +10,19 @@ const Projects = () => {
             <h1 className='heading'>Projects & Work</h1>
             <div className='projects'>
                 <Project
-                    title='Roles Bot'
-                    website='https://roles.bot'
-                    bg='#865EDF'
+                    title='Community Architects'
+                    website='https://community-architects.net'
+                    github='https://github.com/communityarchitects'
+                    bg='#2F975F'
                     size='7'
-                    img={require('../assets/projects/roles-bot.svg').default}
+                    img={require('../assets/projects/community-architects.svg').default}
                 >
-                    The Roles Discord Bot offers unique features for managing a Discord server's roles and is used on more than 2,000 servers. Since 2021 I'm working on this project together with <a target='_blank' rel='noreferrer' href='https://leon.how' >Leon</a>.
+                    Bringing different community leaders from all platforms across the board together into one place. In 2021, I joined the team and developed their website and Discord bots.
                 </Project>
                 <Project
                     title='Discord Bot Template'
                     github='https://github.com/ppauel/typescript-discord-bot'
-                    bg='#5865F2'
+                    bg='#db9053'
                     size='3'
                 >
                     A TypeScript-based multi-purpose Discord template featuring various handlers.
@@ -32,20 +33,19 @@ const Projects = () => {
                 <Project
                     title='Naxite'
                     website='https://nax.gg'
-                    bg='#37affa'
+                    bg='#438be8'
                     size='2'
                 >
-                    A commissioned homepage for the Community Manager Naxite.
+                    A <b>commissioned</b> homepage for the community manager Naxite.
                 </Project>
                 <Project
-                    title='Community Architects'
-                    website='https://community-architects.net'
-                    github='https://github.com/communityarchitects'
-                    bg='#2F975F'
+                    title='Roles Bot'
+                    website='https://roles.bot'
+                    bg='#865EDF'
                     size='8'
-                    img={require('../assets/projects/community-architects.svg').default}
+                    img={require('../assets/projects/roles-bot.svg').default}
                 >
-                    A community bringing together different community leaders from all platforms across the board into one place. I joined the team in 2021 and developed their website and Discord Bots.
+                    The Roles Discord Bot offers unique features for managing a Discord server's roles and is used on more than 2,000 servers. Since 2021 I'm working on this project together with <a target='_blank' rel='noreferrer' href='https://leon.how' >Leon</a>.
                 </Project>
             </div>
         </>
