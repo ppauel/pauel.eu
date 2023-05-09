eb894dfc23d3900224facee289b12159fcaadef4
ppauel <mail@pauel.eu>
2023-05-09 15:34:16 +0200

    Updated "About Me" section

--- src/sections/About.jsx
+++ src/sections/About.jsx
@@ -1,4 +1,5 @@
 import Color from "../components/Color";
+import '../style/about.css';
 
 /**
  * About Me Section
@@ -7,11 +8,14 @@ const About = () => {
     return (
         <>
             <h1 className='heading'>About Me</h1>
-            <p style={{ marginBottom: '100px' }} className='description'>
-                I'm a developer from Germany. I create <Color code='#ddb7ed'>websites</Color> based on <Color>React.js</Color> or <Color>PHP</Color>.
-                My <Color code='#ddb7ed'>bots</Color> & <Color code='#ddb7ed'>applications</Color> are primarily written in <Color>TypeScript</Color>.
-                Apart from that, I occasionally attempt creating <Color code='#ddb7ed'>3D graphic design</Color> and <Color code='#ddb7ed'>music</Color>.
-            </p>
+            <div className="about">
+                <p className="description">
+                    I'm Paul, an 18 years old developer from Germany. I have experience in creating <Color code='#ddb7ed'>websites</Color> based on <Color>React.js</Color> or <Color>PHP</Color>.
+                    My <Color code='#ddb7ed'>bots & applications</Color> are primarily written in <Color>TypeScript</Color>.
+                    Apart from that, I occasionally attempt creating 3D <Color code='#ddb7ed'>graphic design</Color> and <Color code='#ddb7ed'>music</Color>.
+                </p>
+                <img draggable={false} className="no-touch" src={require('../assets/profile.png')} />
+            </div>
         </>
     )
 }
