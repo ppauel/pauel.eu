88290f32aa5dc393d6c1d07530afeac8feb3e275
ppauel <mail@pauel.eu>
2023-06-07 16:18:47 +0200

    Added Behance link

--- src/sections/About.jsx
+++ src/sections/About.jsx
@@ -9,11 +9,18 @@ const About = () => {
         <>
             <h1 className='heading'>About Me</h1>
             <div className="about">
-                <p className="description">
-                    I'm Paul, an 18 years old developer from Germany. I have experience in creating <Color code='#ddb7ed'>websites</Color> based on <Color>React.js</Color> or <Color>PHP</Color>.
-                    My <Color code='#ddb7ed'>bots & applications</Color> are primarily written in <Color>TypeScript</Color>.
-                    Apart from that, I occasionally attempt creating 3D <Color code='#ddb7ed'>graphic design</Color> and <Color code='#ddb7ed'>music</Color>.
-                </p>
+                <div>
+                    <p className="description">
+                        I'm Paul, an 18 years old developer from Germany. I have experience in creating <Color code='#ddb7ed'>websites</Color> based on <Color>React.js</Color> or <Color>PHP</Color>.
+                        My <Color code='#ddb7ed'>bots & applications</Color> are primarily written in <Color>TypeScript</Color>.
+                        Apart from that, I occasionally attempt creating 3D <Color code='#ddb7ed'>graphic design</Color> and <Color code='#ddb7ed'>music</Color>.
+                    </p>
+                    <div className="external">
+                        <a href="https://www.behance.net/pauel" target="_blank" tabIndex={-1}>
+                            <button className="secondary" tabIndex={0}>View on Behance</button>
+                        </a>
+                    </div>
+                </div>
                 <img draggable={false} className="no-touch" src={require('../assets/profile.png')} />
             </div>
         </>
