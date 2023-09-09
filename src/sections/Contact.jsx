760300afbf01bdaab2fe41afdb1652fcf916dd0c
ppauel <mail@pauel.eu>
2023-09-09 21:18:35 +0200

    Fix Discord invite

--- src/sections/Contact.jsx
+++ src/sections/Contact.jsx
@@ -14,7 +14,7 @@ const Contact = () => {
                     <ContactIcon tooltip='Mail' destination='mailto:hello@pauel.eu' img={require('../assets/social/mail.svg').default} />
                     <ContactIcon tooltip='GitHub' destination='https://github.com/ppauel' img={require('../assets/social/github.svg').default} />
                     <ContactIcon tooltip='X' destination='https://twitter.com/_pauel' img={require('../assets/social/twitter.svg').default} />
-                    <ContactIcon tooltip='Discord' destination='https://discord.gg/Un86EhSaQ8' img={require('../assets/social/discord.svg').default} />
+                    <ContactIcon tooltip='Discord' destination='https://discord.gg/XQGrewKZ5Y' img={require('../assets/social/discord.svg').default} />
                 </div>
             </AnimationOnScroll>
         </div>
