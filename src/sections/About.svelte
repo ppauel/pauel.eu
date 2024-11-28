a0e8d89fb0b876ba90ce173e5f588dc8469d5b21
ppauel <82803315+ppauel@users.noreply.github.com>
2024-11-28 12:58:28 +0100

    Update texts

--- src/sections/About.svelte
+++ src/sections/About.svelte
@@ -8,13 +8,9 @@
 <section id="about">
   <h1>About Me</h1>
   <p>
-    I'm Paul, an 19-year-old CompSci student living in Germany. I have an
-    interest in web and app development, often delving into freelance projects
-    alongside my work and studies. Beyond that, I enjoy exploring <a
-      href={Socials.dribble}
-      target="_blank"
-      rel="noreferrer">graphic design</a
-    >
-    and music production.
+    I'm Paul, a 19 year old computer science student living in Germany. I have
+    an interest in web and app development, often delving into freelance
+    projects alongside my work and studies. Beyond that, I enjoy exploring
+    graphic design and music production.
   </p>
 </section>
